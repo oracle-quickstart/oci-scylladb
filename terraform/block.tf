74413baa838963e9424424635137de4b311d7cee
Collin Poczatek <collin.poczatek@gmail.com>
2019-08-20 12:37:27 -0700

    fix mod logic

--- terraform/block.tf
+++ terraform/block.tf
@@ -9,6 +9,6 @@ resource "oci_core_volume" "NodeVolume" {
 resource "oci_core_volume_attachment" "NodeAttachment" {
   count           = "${var.node_count * var.disk_count}"
   attachment_type = "iscsi"
-  instance_id     = "${oci_core_instance.node.*.id[count.index]}"
+  instance_id     = "${oci_core_instance.node.*.id[count.index % var.node_count]}"
   volume_id       = "${oci_core_volume.NodeVolume.*.id[count.index]}"
 }
