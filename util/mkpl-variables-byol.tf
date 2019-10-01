0e7ff492f31015c8fad543686dd2230d66906a52
Collin Poczatek <collin.poczatek@gmail.com>
2019-10-01 11:47:24 -0700

    listing vars

--- util/mkpl-variables-byol.tf
+++ util/mkpl-variables-byol.tf
@@ -3,13 +3,13 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 variable "mp_listing_id" {
-  default = ""
+  default = "ocid1.appcataloglisting.oc1..aaaaaaaa22bdx7mhjeufwilfy5q3p3w4frkp24p5whuiaypxw4gxf2mw3ehq"
 }
 variable "mp_listing_resource_id" {
-  default = ""
+  default = "ocid1.image.oc1..aaaaaaaar24gq5efr23vmsf5zayf6lcys4ggegg3xbat3htpoubyprmre7ra"
 }
 variable "mp_listing_resource_version" {
- default = ""
+ default = "1.0"
 }
 
 variable "use_marketplace_image" {
